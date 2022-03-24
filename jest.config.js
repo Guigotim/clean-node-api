705e2c9ffcbd52d9c7bd87c4a48f38d2ea6e9e8f
Guilherme Roulim Gotin <gui.gotim@hotmail.com>
2022-03-24 13:06:32 -0300

    refactor: make SignUpController conforms to a protocol interface controller

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   roots: ['<rootDir>'],
+  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/interfaces',
+    '/helpers',
+    '/errors',
+  ],
 }
