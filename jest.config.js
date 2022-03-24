0e7ad2542165b4c7e2c5fe700afd79b9808965aa
Guilherme Roulim Gotin <gui.gotim@hotmail.com>
2022-03-23 22:26:10 -0300

    fix: jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
-export default {
+module.exports = {
   roots: ['<rootDir>'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 }
